2019fdff0531e90e0ea3bc4669637a0dad460d58
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-01-07 14:10:48 +0100

    first commit: poc reading xml

---
+++ src/app/utils/dropbox.ts
@@ -0,0 +1,7 @@
+export class Dropbox {
+  static readonly DROPBOX_TOKEN = '***REMOVED***';
+  static readonly DROPBOX_FOLDER = '/MyMovies/';
+  static readonly DROPBOX_USER_FILE = 'user.json';
+  static readonly DROPBOX_FILE_PREFIX = 'ex_';
+  static readonly DROPBOX_FILE_SUFFIX = '.json';
+}
