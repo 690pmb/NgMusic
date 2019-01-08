13ed7c30d1ac52bf66c86fa7a8b0b439cd92eb27
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-01-08 13:51:48 +0100

    update dropbox properties

--- src/app/utils/dropbox.ts
+++ src/app/utils/dropbox.ts
@@ -1,7 +1,6 @@
 export class Dropbox {
   static readonly DROPBOX_TOKEN = '***REMOVED***';
-  static readonly DROPBOX_FOLDER = '/MyMovies/';
-  static readonly DROPBOX_USER_FILE = 'user.json';
-  static readonly DROPBOX_FILE_PREFIX = 'ex_';
+  static readonly DROPBOX_FOLDER = '/XML/';
+  static readonly DROPBOX_FINAL_FILE = 'final.xml';
   static readonly DROPBOX_FILE_SUFFIX = '.json';
 }
