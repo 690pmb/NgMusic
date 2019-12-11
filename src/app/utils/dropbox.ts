7c1d48d38c9f46d2ad50d1cb76cd431d71cb6f8d
pmb <pm.broca@gmail.com>
2019-12-11 18:24:13 +0100

    feat: read zip file & remove author/publish fields

--- src/app/utils/dropbox.ts
+++ src/app/utils/dropbox.ts
@@ -2,5 +2,5 @@ export class Dropbox {
   static readonly DROPBOX_TOKEN = '***REMOVED***';
   static readonly DROPBOX_FOLDER = '/XML/';
   static readonly DROPBOX_FINAL_FILE = 'final';
-  static readonly DROPBOX_FINAL_EXTENTION = '.xml';
+  static readonly DROPBOX_FINAL_EXTENTION = '.zip';
 }
