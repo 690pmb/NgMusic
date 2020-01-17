a9e3ac9f419c39483c1adb95fab3f3d77365d61e
Pierre-Marie Broca <pierre-marie.broca@atos.net>
2020-01-17 16:57:14 +0100

    feat: download fichier data

--- src/app/utils/dropbox.ts
+++ src/app/utils/dropbox.ts
@@ -1,6 +1,7 @@
 export class Dropbox {
   static readonly DROPBOX_TOKEN = '***REMOVED***';
   static readonly DROPBOX_FOLDER = '/XML/';
-  static readonly DROPBOX_FINAL_FILE = 'final';
-  static readonly DROPBOX_FINAL_EXTENTION = '.zip';
+  static readonly DROPBOX_COMPOSITION_FILE = 'final';
+  static readonly DROPBOX_FICHIER_FILE = 'fichier';
+  static readonly DROPBOX_EXTENTION = '.zip';
 }
