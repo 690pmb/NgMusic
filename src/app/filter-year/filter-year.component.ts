63a41a6df2a989e7e7500a39f9c36b5d96cbaf44
69pmb <pmbroca@gmail.com>
2024-02-24 00:21:31 +0100

    chore: standalone

--- src/app/filter-year/filter-year.component.ts
+++ src/app/filter-year/filter-year.component.ts
@@ -1,10 +1,21 @@
 import {Component, Input} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-filter-year',
   templateUrl: './filter-year.component.html',
   styleUrls: ['./filter-year.component.scss'],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+  ],
 })
 export class FilterYearComponent {
   @Input()
--- src/app/row-menu/row-menu.component.ts
+++ src/app/row-menu/row-menu.component.ts
@@ -1,11 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {Composition} from '@utils/model';
 import {MenuService} from '../services/menu.service';
+import {MenuComponent} from '../menu/menu.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-row-menu',
   templateUrl: './row-menu.component.html',
   styleUrls: ['./row-menu.component.scss'],
+  standalone: true,
+  imports: [MatButtonModule, MatMenuModule, FontAwesomeModule, MenuComponent],
 })
 export class RowMenuComponent {
   @Input()
